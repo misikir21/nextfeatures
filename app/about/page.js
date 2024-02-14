4497931d1a3690f7d4949a100a640adc7d4267a0
misikir21 <tekamisikir.com>
2024-02-14 09:21:51 +0300

    add metadata to page

--- app/about/page.js
+++ app/about/page.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+export const metadata = {
+  title: "this next js finest featue which makes your page more decriptive",
+};
 function page() {
   return <div>about</div>;
 }
