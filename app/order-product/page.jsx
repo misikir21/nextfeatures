5d914d420ee54b3a17a4b30f4dc36566a0fff517
misikir21 <tekamisikir.com>
2024-02-15 10:21:15 +0300

    fix all errors

--- app/order-product/page.jsx
+++ app/order-product/page.jsx
@@ -1,17 +1,16 @@
 "use client";
-import React from "react";
 import { useRouter } from "next/navigation"; //this used to define progtamtically way to navigate between pages
 function orderproduct() {
+  const router = useRouter();
   const handleoder = () => {
     console.log("you added an item to the cart");
-    const router = useRouter();
     router.push("/"); //where you want to take the page
   };
 
   return (
     <div>
       <h2>order product</h2>
-      <button>{handleoder}Order Now</button>
+      <button onClick={handleoder}>Order Now</button>
     </div>
   );
 }
