1e4b1b71d9e77c0708c5a2f3fb9d36d7e3153682
misikir21 <tekamisikir.com>
2024-02-15 10:06:23 +0300

    Add userouter ot navigate to oder page

--- app/about/page.js
+++ app/about/page.js
@@ -1,9 +1,10 @@
+import styles from "./"
 import React from "react";
 export const metadata = {
   title: "this next js finest featue which makes your page more decriptive",
 };
 function page() {
-  return <div>about</div>;
+  return <div className={}>about</div>;
 }
 
 export default page;
---
+++ app/order-product/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useRouter } from "./next/navigation";
+function orderproduct() {
+  const handleoder = {};
+  const router = useRouter;
+  router.push("/");
+  return (
+    <div>
+      <h2>order product</h2>
+      <button>{handleoder}Order Now</button>
+    </div>
+  );
+}
+
+export default page;
