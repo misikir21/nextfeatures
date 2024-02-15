144e837ae59cb95402627c13015a5bbdcc58c77f
misikir21 <tekamisikir.com>
2024-02-15 10:10:04 +0300

    fix typos

--- app/about/page.js
+++ app/about/page.js
@@ -1,10 +1,9 @@
-import styles from "./"
 import React from "react";
 export const metadata = {
   title: "this next js finest featue which makes your page more decriptive",
 };
 function page() {
-  return <div className={}>about</div>;
+  return <div>about</div>;
 }
 
 export default page;
